795009eaaefb6ecc7a902a738da65f9f5c29d5f1
З - 16 < - 16@DESKTOP-8ICTQ04>
2024-05-13 21:14:29 +0500

    Добавила код с второго занятия

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,14 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+//Заполнение массива случайными числами
+template <typename T>
+void fill_arr(T arr[], const int length, int left, int right) {
+    srand(time(NULL));
+        for (int i = 0; i < length; i++)
+            arr[i] = rand() % (right - left) + left;
+
+}
 
 
 //Вывод массива в консоль
@@ -11,9 +21,17 @@ void print_arr(T arr[], const int length) {
 }
 
 
+//Задача1  Создайте функцию, к.я принимает динамический массив, его длину и число. Длина переданного массива должна увеличиться на переданное значение.
+template <typename T>
+void arr_app(T*& arr, int& length, int num);
+//Задача2
+template <typename T>
+void arr_pop(T*& arr, int& length, int num);
+
+
 int main(){
     setlocale(LC_ALL, "ru");
-    int n;
+    int n,m;
 
 
     //Выделение динамической памяти под переменные
@@ -36,7 +54,7 @@ int main(){
 
 
     //Динамические массивы
-    std::cout << " Введите размер массива -> ";
+    /*std::cout << " Введите размер массива -> ";
     int dsize;
     std::cin >> dsize;
     int* darr = new int[5];
@@ -49,6 +67,105 @@ int main(){
     std::cout << "Итоговый массив:\n";
     print_arr(darr, dsize);
 
+    delete[] darr;
+*/
 
+
+    //Двумерные динамические массивы
+    int rows = 5, cols = 6;
+    // int* mx = new int[rows][cols];// НЕ РАБОТАЕТ
+    
+    //Выделение памяти
+    int** mx = new int* [rows];
+    for (int i = 0; i < rows; i++)
+        mx[i] = new int[cols];
+
+    //Освобождение памяти 
+    for (int i = 0; i < rows; i++)
+        delete[] mx[i];
+    delete[] mx;
+
+    //Области применения динамической памяти. 
+    // 1. Создание массива размер которого может 
+    // быть на этапе выполнения программы;
+    // 2. Создание массива размер которого должен иметь возможность измениться;
+    // 3. Создание связных динамических структур данных;
+    // 4. Выделение памяти для объектов в случае их крайней необходимости.
+
+    // Задача 1. Увеличение размера массива.
+/*
+    std::cout << "Задача1. \nВведите размер массива -> ";
+    int size1;
+    std::cin >> size1;
+    int* arr1 = new int[size1];
+    fill_arr(arr1, size1, 10, 21);
+    std::cout << "Изначальный массив:\n";
+    print_arr(arr1, size1);
+    std::cout << "Введите количество новых элементов -> ";
+    std::cin >> n;
+
+    arr_app(arr1, size1, n);
+    std::cout << "Итоговый массив:\n";
+    print_arr(arr1, size1);
+*/
+// Задача 2. Уменьшение размера массива
+    std::cout << "Задача2. \nВведите размер массива -> ";
+  
+    int size2;
+    std::cin >> size2;
+    int* arr2 = new int[size2];
+    fill_arr(arr2, size2, 10, 21);
+    std::cout << "Изначальный массив:\n";
+    print_arr(arr2, size2);
+    std::cout << "Введите количество элементов для удаления -> ";
+    std::cin >> m;
+
+    arr_pop(arr2, size2, m);// 
+    std::cout << "Итоговый массив:\n";
+    print_arr(arr2, size2);
     return 0;
+}
+
+
+
+template <typename T>
+void arr_app(T*& arr, int& length, int num) {
+    //Шаг 0. Проверка на количество элементов для увеличения размера массива
+    if (num <= 0)
+        return;
+   //Шаг 1. Создание нового динамического массива, нужного размера
+    T* tmp = new T[length + num]{};
+
+   //Шаг 2. Копирование значений старого массива в элементы нового массива.
+    for (int i = 0; i < length; i++)
+        tmp[i] = arr[i];
+
+    //Шаг 3. Удаление старого массива
+    delete[] arr;
+
+    //Шаг 4. Перенаправление указателя на новый массив.
+    arr = tmp;
+
+    //Шаг 5. Увеличение переменной, отвечающей за размер массива
+    length += num;
+
+}
+
+
+template <typename T>
+void arr_pop(T*& arr, int& length, int num) {
+    if (length <= 0)
+        return;
+
+    if (num >= length) {
+        delete[] arr;
+        arr = nullptr;
+        return;
+    }
+    T* tmp = new T[length - num];
+    for(int i = 0; i < length - num; i++)
+        tmp[i] = arr[i];
+    delete[] arr;
+    arr = tmp;
+    length -= num;
 }
